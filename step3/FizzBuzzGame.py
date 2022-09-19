8633c258fdcfbe5fff7b18cd15f02217cc41d9d7
levkesha <zxcv02@gmail.com>
2022-09-19 19:45:35 +0300

    final

--- step3/FizzBuzzGame.py
+++ step3/FizzBuzzGame.py
@@ -2,11 +2,11 @@ def get_rules(self):
     return self.conditions
 
 
-def set_rules(self, rules):
-    self.conditions = rules
+def set_rules(self, conditions):
+    self.conditions = conditions
 
 
 class FizzBuzzGame:
-    def __init__(self, rules):
-        self.rules = rules
+    def __init__(self, conditions):
+        self.rules = conditions
 
