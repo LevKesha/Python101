35e463f167ac72a7f89708aa666c14929c465149
Lev Goldenberg <zxcv02@gmail.com>
2022-10-03 12:40:56 +0300

    Update FizzBuzzGame.py

--- step3/FizzBuzzGame.py
+++ step3/FizzBuzzGame.py
@@ -1,12 +1,13 @@
-def get_conditions(self):
-    return self.conditions
-
-
-def set_conditions(self, conditions):
-    self.conditions = conditions
-
-
 class FizzBuzzGame:
     def __init__(self, conditions):
         self.conditions = conditions
+
+    def get_conditions(self):
+    return self.conditions
+
+
+    def set_conditions(self, conditions):
+    self.conditions = conditions
+
+
 
