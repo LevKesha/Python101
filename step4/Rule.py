2841a88dfcad6fccf5d98214fb527d3aff9339ff
Lev Goldenberg <zxcv02@gmail.com>
2022-10-03 12:41:37 +0300

    Update Rule.py

--- step4/Rule.py
+++ step4/Rule.py
@@ -1,20 +1,16 @@
-def get_first(self):
-    return self.first
-
-
-def get_second(self):
-    return self.second
-
-
-def get_condition(self):
-    return self.condition
-
-
 class Rule:
     def __init__(self, first, second, condition):
         self.first = first
         self.second = second
         self.condition = condition
 
+    def get_first(self):
+    return self.first
 
 
+    def get_second(self):
+    return self.second
+
+
+    def get_condition(self):
+    return self.condition
